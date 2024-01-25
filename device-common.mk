32b0cc9e089684d15445ab769f239e10cda3d622
Lunarixus <Nathan@lunarixus.dev>
2024-01-25 01:49:54 +0000

    sm8250-common: Build atrace service
    Signed-off-by: Lunarixus <Nathan@lunarixus.dev>

--- device-common.mk
+++ device-common.mk
@@ -13,6 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Atrace
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service
+
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
